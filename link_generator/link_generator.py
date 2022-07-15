d42f69f16443edb531fd7770859a797b5e1aa1c0
Maxipanza <78429597+Maxipanza@users.noreply.github.com>
2022-07-15 15:31:23 -0300

    deployment v2
    ya no hago una lista en base al archivo, sino que recorro todo y mando a la lista solo los que terminen en cierto formato.
    
    Ahora deberia funcionar en cualquier computadora

--- link_generator/link_generator.py
+++ link_generator/link_generator.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 from pathlib import Path
 
 
@@ -6,25 +7,29 @@ dir_actual = os.path.dirname(os.path.abspath(__file__))
 
 dir_final=dir_actual.replace('\\','/')
 
-gif_list = [ archivo.as_posix() for archivo in Path(dir_actual).glob('**/imagen-interactiva/*')]
-fotoagencia_list = [ archivo.as_posix() for archivo in Path(dir_actual).glob('**/foto-agencia/*')]
-color_list = [ archivo.as_posix() for archivo in Path(dir_actual).glob('**/colores/*')]
-fichatecnica_list = [ archivo.as_posix() for archivo in Path(dir_actual).glob('**/ficha-tecnica/*')]
+gif_list = [ archivo.with_suffix(".gif").as_posix() for archivo in Path(dir_actual).glob('**/*')]
+jpg_list = [ archivo.with_suffix(".jpg").as_posix() for archivo in Path(dir_actual).glob('**/*')]
+png_list = [ archivo.with_suffix(".png").as_posix() for archivo in Path(dir_actual).glob('**/*')]
+jpeg_list = [ archivo.with_suffix(".jpeg").as_posix() for archivo in Path(dir_actual).glob('**/*')]
+pdf_list = [ archivo.with_suffix(".pdf").as_posix() for archivo in Path(dir_actual).glob('**/*')]
 
 link = "https://www.landinginteligente.com/fotos"
 
 gif_final = [s.replace(dir_final, link) for s in gif_list]
-fotoagencia_final = [s.replace(dir_final, link) for s in fotoagencia_list]
-color_final = [s.replace(dir_final, link) for s in color_list]
-fichatecnica_final = [s.replace(dir_final, link) for s in fichatecnica_list]
+jpg_final = [s.replace(dir_final, link) for s in jpg_list]
+png_final = [s.replace(dir_final, link) for s in png_list]
+jpeg_final = [s.replace(dir_final, link) for s in jpeg_list]
+pdf_final = [s.replace(dir_final, link) for s in pdf_list]
 
 
 with open(os.path.join(os.path.expanduser('~'),'Desktop','Links generados.txt'), "w") as outfile:
     outfile.writelines("\n".join(str(item) for item in gif_final) + '\n')
     outfile.writelines("\n")
-    outfile.writelines("\n".join(str(item) for item in fotoagencia_final) + '\n')
+    outfile.writelines("\n".join(str(item) for item in jpg_final) + '\n')
     outfile.writelines("\n")
-    outfile.writelines("\n".join(str(item) for item in color_final) + '\n')
+    outfile.writelines("\n".join(str(item) for item in png_final) + '\n')
     outfile.writelines("\n")
-    outfile.writelines("\n".join(str(item) for item in fichatecnica_final) + '\n')
+    outfile.writelines("\n".join(str(item) for item in jpeg_final) + '\n')
+    outfile.writelines("\n")
+    outfile.writelines("\n".join(str(item) for item in pdf_final) + '\n')
 
