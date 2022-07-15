c46b5900de633cb6d702ba40085757abee1bfb1c
Maxipanza <78429597+Maxipanza@users.noreply.github.com>
2022-07-15 15:32:15 -0300

    full deployment

--- link_generator/link_generator.py
+++ link_generator/link_generator.py
@@ -2,34 +2,12 @@ import os
 import pathlib
 from pathlib import Path
 
-
 dir_actual = os.path.dirname(os.path.abspath(__file__))
-
 dir_final=dir_actual.replace('\\','/')
-
-gif_list = [ archivo.with_suffix(".gif").as_posix() for archivo in Path(dir_actual).glob('**/*')]
-jpg_list = [ archivo.with_suffix(".jpg").as_posix() for archivo in Path(dir_actual).glob('**/*')]
-png_list = [ archivo.with_suffix(".png").as_posix() for archivo in Path(dir_actual).glob('**/*')]
-jpeg_list = [ archivo.with_suffix(".jpeg").as_posix() for archivo in Path(dir_actual).glob('**/*')]
-pdf_list = [ archivo.with_suffix(".pdf").as_posix() for archivo in Path(dir_actual).glob('**/*')]
+archivos_list = [ archivo.as_posix() for archivo in Path(dir_actual).glob('**/*')]
 
 link = "https://www.landinginteligente.com/fotos"
 
-gif_final = [s.replace(dir_final, link) for s in gif_list]
-jpg_final = [s.replace(dir_final, link) for s in jpg_list]
-png_final = [s.replace(dir_final, link) for s in png_list]
-jpeg_final = [s.replace(dir_final, link) for s in jpeg_list]
-pdf_final = [s.replace(dir_final, link) for s in pdf_list]
-
-
-with open(os.path.join(os.path.expanduser('~'),'Desktop','Links generados.txt'), "w") as outfile:
-    outfile.writelines("\n".join(str(item) for item in gif_final) + '\n')
-    outfile.writelines("\n")
-    outfile.writelines("\n".join(str(item) for item in jpg_final) + '\n')
-    outfile.writelines("\n")
-    outfile.writelines("\n".join(str(item) for item in png_final) + '\n')
-    outfile.writelines("\n")
-    outfile.writelines("\n".join(str(item) for item in jpeg_final) + '\n')
-    outfile.writelines("\n")
-    outfile.writelines("\n".join(str(item) for item in pdf_final) + '\n')
-
+archivos_final = [s.replace(dir_final, link) for s in archivos_list if s.endswith((".txt", ".pdf", ".jpg", ".jpeg", ".png", ".gif"))]
+with open(os.path.join(os.path.join(os.environ['USERPROFILE']),'Links generados.txt'), "w") as outfile:
+    outfile.writelines("\n".join(str(item) for item in archivos_final) + '\n')
