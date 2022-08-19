888d3fd48d9d42941eb2910e14dff8502a8f7aa9
Maxipanza <78429597+Maxipanza@users.noreply.github.com>
2022-08-19 16:55:05 -0300

    tutorial 5

--- run/mysite/polls/models.py
+++ run/mysite/polls/models.py
@@ -11,8 +11,9 @@ class Question (models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
-    def fue_publicado_recientemente(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
