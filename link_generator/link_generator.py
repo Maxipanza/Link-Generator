f809b057fe4fda1a25b5ae44339cf322faf18af3
Maxipanza <78429597+Maxipanza@users.noreply.github.com>
2022-10-19 14:02:34 -0300

    mejorado el generador de links

--- link_generator/link_generator.py
+++ link_generator/link_generator.py
@@ -2,12 +2,31 @@ import os
 import pathlib
 from pathlib import Path
 
-dir_actual = os.path.dirname(os.path.abspath(__file__))
-dir_final=dir_actual.replace('\\','/')
-archivos_list = [ archivo.as_posix() for archivo in Path(dir_actual).glob('**/*')]
+def obtener_path_del_escritorio():
+    return os.path.join(os.path.join(os.environ['USERPROFILE'], 'Escritorio'))
 
-link = "https://www.landinginteligente.com/fotos"
+def obtener_directorio_actual():
+    return os.path.dirname(os.path.abspath(__file__))
 
-archivos_final = [s.replace(dir_final, link) for s in archivos_list if s.endswith((".txt", ".pdf", ".jpg", ".jpeg", ".png", ".gif"))]
-with open(os.path.join(os.path.join(os.environ['USERPROFILE']),'Links generados.txt'), "w") as outfile:
-    outfile.writelines("\n".join(str(item) for item in archivos_final) + '\n')
+def todos_los_paths_del_directorio_y_sus_subdirectorios(directorio):
+    return [archivo.as_posix() for archivo in Path(directorio).glob('**/*')]
+
+def obtener_paths_de_archivos():
+     return todos_los_paths_del_directorio_y_sus_subdirectorios(obtener_directorio_actual())
+     
+def _raiz_del_link():
+    return "https://www.landinginteligente.com/fotos"
+
+def links_de_archivos(paths_de_archivos):
+    dir_final=obtener_directorio_actual().replace('\\','/')
+    archivos_final = [path.replace(dir_final, _raiz_del_link()) for path in paths_de_archivos if s.endswith((".txt", ".pdf", ".jpg", ".jpeg", ".png", ".gif"))]
+    return archivos_final
+
+def guardar_los_links_en_un_archivo(links):
+    with open(obtener_path_del_escritorio(),'Links generados.txt'), "w" as outfile:
+        outfile.writelines("\n".join(str(link) for link in links) + '\n')
+
+archivos = obtener_paths_de_archivos(links_de_archivos())
+links = links_de_archivos()
+guardar_los_links_en_un_archivo(links)
+
